fc597c90bca70f4c089827732e98a7fcd5c620e5
Vishal Chand <163511305+bornpresident@users.noreply.github.com>
2025-03-04 11:07:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from perform_ocr import pdf_to_txt
+import sys
 
+# Default parameters
 input_file = 'data/input/1.pdf'
 outputsetname = '2-nms'
 lang = 'eng'
-pdf_to_txt(input_file, outputsetname, lang)
+export_tensor = False  # Default: don't export tensor
+export_onnx = False    # Default: don't export to ONNX
+
+# Parse command line arguments if provided
+if len(sys.argv) > 1:
+    input_file = sys.argv[1]
+if len(sys.argv) > 2:
+    outputsetname = sys.argv[2]
+if len(sys.argv) > 3:
+    lang = sys.argv[3]
+if len(sys.argv) > 4:
+    export_tensor = sys.argv[4].lower
